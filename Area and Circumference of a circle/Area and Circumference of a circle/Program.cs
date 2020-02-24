45f5823507d2e149d78600d4b5369afb7f62dbf5
Diego Marquez <59837903+diegomarquez-real@users.noreply.github.com>
2020-02-24 09:32:52 -0500

    Add files via upload

---
+++ Area and Circumference of a circle/Area and Circumference of a circle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_and_Circumference_of_a_circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1
+            // Partially worked example
+            Console.WriteLine("\nPart 1, circumference and area of a circle.");
+            Console.Write("Enter an integer for the radius: ");
+            string strradius = Console.ReadLine();
+            int intradius = int.Parse(strradius);
+            double circumference = 2 * Math.PI * intradius;
+            Console.WriteLine($"The circumference is {circumference});
+    }
+}
